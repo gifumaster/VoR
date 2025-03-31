01096fd717a93b5f0ad5cb11a9053a5edda760d7
gifumaster <gifumaster@gmail.com>
2025-03-31 21:06:08 +0900

    padding 調整

--- src/lib/CardGrid.svelte
+++ src/lib/CardGrid.svelte
@@ -21,6 +21,7 @@
     grid-template-columns: repeat(auto-fill, minmax(200px, 250px));
     gap: 1rem;
     padding: 1rem;
+    padding-top:0;
     width: 100%;
     justify-content: center;
     align-content: start;    /* グリッド全体を上寄せ */
