1827ea1c6cbae49f487e21fc7d0671c53fcd9ec9
gifumaster <gifumaster@gmail.com>
2025-04-02 21:29:42 +0900

    test

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: '/VoR/', // リポジトリ名に基づいたベースパスを設定
+  base: process.env.VITE_BASE_PATH || '/VoR/', // リポジトリ名に基づいたベースパスを設定
   build: {
     outDir: 'docs' // GitHub Pagesのデフォルトディレクトリ
   }
